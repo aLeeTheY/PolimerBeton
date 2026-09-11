55c6af7fd7bb65be226140c234180860190643c3
Vladimir Nikulin <aleethey@gmail.com>
2026-09-11 15:09:01 +0300

    chore(gulp, scripts): commend unused code

--- gulp/tasks/scripts/scripts.js
+++ gulp/tasks/scripts/scripts.js
@@ -1,5 +1,5 @@
 import gulp from 'gulp'
-import gulpReplace from 'gulp-replace'
+// import gulpReplace from 'gulp-replace'
 import browserSync from 'browser-sync'
 
 import { env } from '../../config/env.js'
